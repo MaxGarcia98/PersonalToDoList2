b5efd0610c9f808798ca0c8028cb7c0714148f46
MaxGarcia98 <75860997+MaxGarcia98@users.noreply.github.com>
2023-10-13 12:21:00 -0400

    Initial Commit

---
+++ PersonalToDoList2/PersonalToDoList2App.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalToDoList2App.swift
+//  PersonalToDoList2
+//
+//  Created by Maximino Garcia II on 10/13/23.
+//
+
+import SwiftUI
+
+@main
+struct PersonalToDoList2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
